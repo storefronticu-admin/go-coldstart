a347c703f20c6f0d9ef69a521875cd2919ea7429
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2024-01-29 16:08:58 +0000

    push

--- base.repoinfo.go
+++ base.repoinfo.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
-	"cloud.google.com/go/firestore/apiv1/firestorepb"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,30 +12,8 @@ import (
 func repoinfoGet(c *gin.Context) {
 	id := c.Query("id")
 	if id == "" {
-		collection := client.Collection("users")
-
-		aggregationQuery := collection.NewAggregationQuery().WithCount("all")
-		results, err := aggregationQuery.Get(ctx)
-		if err != nil {
-			errorLog.Printf("Error querying: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error: %v", err)})
-			return
-		}
-
-		count, ok := results["all"]
-		if !ok {
-			errorLog.Println("Firestore: couldn't get alias for COUNT from results")
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving count"})
-			return
-		}
-
-		countValue := count.(*firestorepb.Value)
-		resultString := fmt.Sprintf("Number of results from query: %d\n", countValue.GetIntegerValue())
-		successLog.Println(resultString)
-
-		// Convert the int64 to a string before writing it to the response
-		c.JSON(http.StatusOK, gin.H{"count": strconv.FormatInt(countValue.GetIntegerValue(), 10)})
-		debugLog.Println("repoinfoHead is not implemented yet")
+		errorLog.Printf("Failed getting specific repo info")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Repository identifier is missing to retrieve document"})
 		return
 	}
 
